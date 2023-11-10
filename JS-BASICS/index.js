09594dfcf5f5ccbd46ce109af995aeafce34b4da
Serdar Amansahedov <bgojayeva@bk.ru>
2023-11-10 15:06:28 +0800

    If...Else Java script

--- JS-BASICS/index.js
+++ JS-BASICS/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+let hour =10;
+
+if (hour >= 6 && hour < 12) 
+    console.log("Good Morning")
+else if (hour >= 12 && hour < 18) 
+    console.log("Good Afternoon");
+else 
+    console.log("Good Evening");
